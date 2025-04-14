5aed2b00d5660d0fc06dac57df23a8aa9e2165b3
Umorilol <99937650+Umorilol@users.noreply.github.com>
2025-04-13 20:33:41 -0400

    Updated Database

--- FlaskDB/schema.sql
+++ FlaskDB/schema.sql
@@ -6,3 +6,20 @@ CREATE TABLE user (
     password TEXT NOT NULL
 );
 
+CREATE TABLE wardrobe (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    user_id INTEGER NOT NULL UNIQUE, 
+    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE clothing_items (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    wardrobe_id INTEGER NOT NULL,
+    image_path TEXT NOT NULL,
+    item_type TEXT,
+    attributes TEXT,
+    embedding BLOB,
+    uploaded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (wardrobe_id) REFERENCESwardrobes(id)
+);
